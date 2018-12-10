984b7e34d331bfe25869209cf355543b345a4f11
Hiyoung.Tsui <992896924@qq.com>
2018-12-10 18:08:39 +0800

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,24 @@
+#! /System/Library/Frameworks/Python.framework python
+import os
+import datetime
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+timestamp = str(datetime.datetime.now().microsecond)
+filetype = ".png"
+filename = timestamp + filetype
+
+os.system("screencapture -i " + filename)
+result = decode(Image.open(filename))
+
+if len(result) == 1:
+  os.system("open -a /Applications/Safari.app " + result[0].data)
+elif len(result) > 1:
+  print("Too many.")
+  for i in result:
+    print(i.data)
+
+try:
+  os.remove(filename)
+except OSError:
+  print("Nothing.")
