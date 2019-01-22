0eb876cc556306de76e6ec7417fe5a1e98961d29
15331335 <992896924@qq.com>
2019-01-22 23:32:49 +0800

    add ocr

--- qr.py
+++ qr.py
@@ -4,12 +4,31 @@ import datetime
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+# baidu ocr api
+from aip import AipOcr
+import pyperclip
+
+APP_ID = '******'
+API_KEY = '******'
+SECRET_KEY = '******'
+
+options = {}
+options["language_type"] = "CHN_ENG"
+options["detect_direction"] = "false"
+options["detect_language"] = "true"
+options["probability"] = "false"
+
+def get_file_content(filePath):
+  with open(filePath, 'rb') as fp:
+    return fp.read()
+
 timestamp = str(datetime.datetime.now().microsecond)
 filetype = ".png"
 filename = timestamp + filetype
 
 os.system("screencapture -i " + filename)
 
+# qr
 try:
   result = decode(Image.open(filename))
 except IOError:
@@ -18,6 +37,16 @@ else:
   if len(result) >= 1:
     for i in result:
       os.system("open -a /Applications/Safari.app " + i.data)
+
+  # ocr
+  else:
+    client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+    image = get_file_content(filename)
+    result = client.basicGeneral(image, options)
+    result_str = ""
+    for i in range(result["words_result_num"]):
+      result_str = result_str + result["words_result"][i]["words"]
+    pyperclip.copy(result_str)
 
   try:
     os.remove(filename)
